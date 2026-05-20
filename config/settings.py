b79b2e97cf34cda2152d96408e1f9ff433abd683
Muhammad-amin-ef <eldorquryozov386@gmail.com>
2026-05-20 11:23:29 +0500

    docker-compose image ga ozgartirildi

--- config/settings.py
+++ config/settings.py
@@ -60,8 +60,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),  # Docker: 'db', Local: 'localhost'
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
